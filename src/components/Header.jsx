b4063debcd3603ee083f06145b2a2f727bcce9f6
AnnaBredikhina <90130551+AnnaBredikhina@users.noreply.github.com>
2024-06-13 12:08:33 +0300

    modified:   src/components/Footer.jsx 	modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ function Header() {
     return <div className="navbar-fixed">
         <nav className="purple lighten-2">
             <div className="nav-wrapper">
+                {/* eslint-disable-next-line  */}
                 <a href="#" className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#">Repo</a></li>
+                    <li><a href="https://github.com/AnnaBredikhina/shop_project">Repo</a></li>
                 </ul>
             </div>
         </nav>
